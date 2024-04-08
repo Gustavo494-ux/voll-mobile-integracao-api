73f32dfacf028c5009acdac1d6c960222e0c5944
Gustavo Gama <gustavogama494@gmail.com>
2024-04-08 17:39:19 -0300

    modulo 3 aula 1

--- App.tsx
+++ App.tsx
@@ -2,8 +2,10 @@ import { NativeBaseProvider, StatusBar } from 'native-base';
 
 import { TEMAS } from './src/estilos/temas';
 import Rotas from './src/Rotas';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function App() {
+  AsyncStorage.removeItem('token')
   return (
     <NativeBaseProvider theme={TEMAS}>
       <StatusBar backgroundColor={TEMAS.colors.blue[800]} />
