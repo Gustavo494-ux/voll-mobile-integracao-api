ebe822421c797223381a0c7bbf3c901f2106bf1a
Gustavo <gustavogama494@gmail.com>
2024-04-15 08:35:25 -0300

    modulo 4 aula 2

--- src/Tabs/Perfil.tsx
+++ src/Tabs/Perfil.tsx
@@ -4,8 +4,10 @@ import { useEffect, useState } from 'react'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { pegarDadosPaciente } from '../servicos/PacienteServico'
 import { Paciente } from '../interfaces/Paciente'
+import { Botao } from '../componentes/Botao'
 
-export default function Perfil(){
+
+export default function Perfil({ navigation }:any){
   const [dadosPaciente, setDadosPaciente] = useState({} as Paciente)
 
   useEffect(() => {
@@ -14,6 +16,7 @@ export default function Perfil(){
       if (!pacienteId) return null
 
       const resultado = await pegarDadosPaciente(pacienteId)
+      console.log(resultado)
       if(resultado){
         setDadosPaciente(resultado)
         console.log(resultado)
@@ -21,6 +24,11 @@ export default function Perfil(){
     }
     dadosPaciente();
   },[])
+
+  function deslogar(){
+    AsyncStorage.multiRemove(['token','pacienteId'])
+    navigation.replace('Login')
+  }
 
   return(
     <ScrollView flex={1}>
@@ -32,7 +40,7 @@ export default function Perfil(){
         <Titulo color="blue.500">Informações pessoais</Titulo>
         <Titulo fontSize="lg" mb={1}>{dadosPaciente.nome}</Titulo>
         <Text>{dadosPaciente.email}</Text>
-        <Text>{dadosPaciente.endereco.estado}</Text>
+        <Text>{dadosPaciente?.endereco?.estado}</Text>
 
         <Divider mt={5} />
 
@@ -42,6 +50,10 @@ export default function Perfil(){
             <Text key={index}>{plano}</Text>
           })
         }        
+
+        <Botao onPress={deslogar}>
+          Deslogar
+        </Botao>
       </VStack>
     </ScrollView>
   )
