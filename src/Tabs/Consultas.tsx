d0b0c39f8dcd704aeb9f59a19e8ca90094262264
Gustavo <gustavogama494@gmail.com>
2024-04-17 08:06:23 -0300

    fim do modulo 5

--- src/Tabs/Consultas.tsx
+++ src/Tabs/Consultas.tsx
@@ -2,46 +2,78 @@ import { VStack, Divider, ScrollView } from 'native-base'
 import { Botao } from '../componentes/Botao'
 import { CardConsulta } from '../componentes/CardConsulta'
 import { Titulo } from '../componentes/Titulo'
+import { useEffect, useState } from 'react'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { pegarConsultasPaciente } from '../servicos/PacienteServico'
+
+interface Especialista{
+  id: string;
+  nome: string;
+  imagem: string;
+  especialidade: string;
+}
+interface Consulta{
+  id: string;
+  data: string;
+  especialista: Especialista; 
+}
 
 export default function Consultas(){
+  const [consultasProximas, setConsultasProximas] = useState<Consulta[]>([])
+  const [consultasPassadas, setConsultasPassadas] = useState<Consulta[]>([])
+
+  useEffect(() => {
+    async function pegarConsultas(){
+      const pacienteId = await AsyncStorage.getItem('pacienteId')
+      if(!pacienteId) return;
+
+      const todasConsultas:Consulta[] = await pegarConsultasPaciente(pacienteId)
+
+      const agora = new Date()
+
+      const proximas = todasConsultas.filter((consulta) => new Date(consulta.data) > agora)
+      const passadas = todasConsultas.filter((consulta) => new Date(consulta.data) <= agora)
+
+      setConsultasPassadas(passadas);
+      setConsultasProximas(proximas);
+    }
+    pegarConsultas()
+  },[])
+
   return(
     <ScrollView p="5">
       <Titulo color="blue.500">Minhas consultas</Titulo>
       <Botao mt={5} mb={5}>Agendar nova consulta</Botao>
 
       <Titulo color="blue.500" fontSize="lg" alignSelf="flex-start" mb={2}>Próximas consultas</Titulo>
-      <CardConsulta 
-        nome='Dr. Andre'
-        especialidade='Cardiologista'
-        foto='https://github.com/andreocunha.png'
-        data='20/04/2023'
-        foiAgendado
-      />
+      {
+        consultasProximas?.map((consulta)=> (
+          <CardConsulta 
+            key={consulta.id}
+            nome={consulta.especialista.nome}
+            especialidade={consulta.especialista.especialidade}
+            foto={consulta.especialista.imagem}
+            data={consulta.data}
+            foiAgendado
+          />
+        ))
+      }
 
       <Divider mt={5} />
 
       <Titulo color="blue.500" fontSize="lg" alignSelf="flex-start" mb={2}>Consultas passadas</Titulo>
-      <CardConsulta 
-        nome='Dr. Andre'
-        especialidade='Cardiologista'
-        foto='https://github.com/andreocunha.png'
-        data='20/04/2023'
-        foiAtendido
-      />
-      <CardConsulta 
-        nome='Dr. Andre'
-        especialidade='Cardiologista'
-        foto='https://github.com/andreocunha.png'
-        data='20/04/2023'
-        foiAtendido
-      />
-      <CardConsulta 
-        nome='Dr. Andre'
-        especialidade='Cardiologista'
-        foto='https://github.com/andreocunha.png'
-        data='20/04/2023'
-        foiAtendido
-      />
+      {
+        consultasPassadas?.map((consulta)=> (
+          <CardConsulta 
+            key={consulta.id}
+            nome={consulta.especialista.nome}
+            especialidade={consulta.especialista.especialidade}
+            foto={consulta.especialista.imagem}
+            data={consulta.data}
+            foiAtendido
+          />
+        ))
+      }
     </ScrollView>
   )
 }
